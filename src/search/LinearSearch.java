a288844c25e6be5618e56095d40c195c4dda459e
ItsAyushSomani <ayush.cci@gmail.com>
2021-03-01 15:29:15 +0530

    DS - Array and Search

---
+++ src/search/LinearSearch.java
@@ -0,0 +1,34 @@
+package search;
+
+/**
+ * In an unsorted array, the search operation can be performed by linear traversal from the first element to the last element.
+ */
+public class LinearSearch {
+
+    public static void main(String[] args) {
+        int arr[] = {12, 34, 10, 6, 40};
+        int n = arr.length;
+
+        // Using a last element as search element
+        int key = 40;
+        int position = linearSearch(arr, key);
+
+        if (position == - 1)
+            System.out.println("Element not found");
+        else
+            System.out.println("Element Found at Position: "
+                    + (position + 1));
+    }
+
+    public static int linearSearch(int[] arr, int toBeSearched) {
+        if (arr.length < 1) {
+            return -1;
+        }
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == toBeSearched) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
