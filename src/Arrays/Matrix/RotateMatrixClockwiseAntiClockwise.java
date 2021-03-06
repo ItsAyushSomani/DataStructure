6a69ad77921f7894c424053d0c06cc92bd79ef40
ItsAyushSomani <ayush.cci@gmail.com>
2021-03-06 13:04:54 +0530

    DS - Array -> Matrix -> MatrixRotation

--- src/Arrays/Matrix/RotateMatrixClockwiseAntiClockwise.java
+++ src/Arrays/Matrix/RotateMatrixClockwiseAntiClockwise.java
@@ -13,11 +13,10 @@ public class RotateMatrixClockwiseAntiClockwise {
 
 
         //test case 2
-//
-//        int arr[][] = { { 1, 2, 3, 4 },
-//                { 5, 6, 7, 8 },
-//                { 9, 10, 11, 12 },
-//                { 13, 14, 15, 16 } };
+        /** int arr[][] = {{1, 2, 3, 4},
+                {5, 6, 7, 8},
+                {9, 10, 11, 12},
+                {13, 14, 15, 16}}; */
 
         rotateAntiClockwiseWithoutExtraspace(arr);
         for (int i = 0; i < arr.length; i++) {
