a7b9c80015f4d1a63445b1682977b07d7c407a23
ItsAyushSomani <ayush.cci@gmail.com>
2021-03-07 10:27:15 +0530

    DS - Array -> Matrix -> MatrixRotation

--- src/Arrays/Matrix/MatrixRotationAntiClockwise.java
+++ src/Arrays/Matrix/MatrixRotationAntiClockwise.java
@@ -7,10 +7,10 @@ package Arrays.Matrix;
 public class MatrixRotationAntiClockwise {
 
     public static void main(String[] args) {
-        int arr[][] = { {1, 2, 3, 4},
+        int arr[][] = {{1, 2, 3, 4},
                 {5, 6, 7, 8},
                 {9, 10, 11, 12},
-                {13, 14, 15, 16} };
+                {13, 14, 15, 16}};
 
         for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < arr[0].length; j++)
@@ -36,15 +36,15 @@ public class MatrixRotationAntiClockwise {
             }
             prev = arr[rowLength - 1][colBegin];
 
-            for(int i= colBegin; i < colLength ; i++) {
-                int curr = arr[rowLength-1][i];
-                arr[rowLength-1][i] = prev;
+            for (int i = colBegin; i < colLength; i++) {
+                int curr = arr[rowLength - 1][i];
+                arr[rowLength - 1][i] = prev;
                 prev = curr;
             }
 
             rowLength--;
 
-            for(int i= rowLength-1; i >= rowBegin ; i--) {
+            for (int i = rowLength - 1; i >= rowBegin; i--) {
                 int curr = arr[i][colLength - 1];
                 arr[i][colLength - 1] = prev;
                 prev = curr;
@@ -52,14 +52,14 @@ public class MatrixRotationAntiClockwise {
             colLength--;
 
 
-            for(int i= colLength-1; i >= colBegin; i--) {
+            for (int i = colLength - 1; i >= colBegin; i--) {
                 int curr = arr[rowBegin][i];
                 arr[rowBegin][i] = prev;
                 prev = curr;
             }
             rowBegin++;
 
-            for(int i = rowBegin; i <= rowLength; i++) {
+            for (int i = rowBegin; i <= rowLength; i++) {
                 int curr = arr[i][colBegin];
                 arr[i][colBegin] = prev;
                 prev = curr;
