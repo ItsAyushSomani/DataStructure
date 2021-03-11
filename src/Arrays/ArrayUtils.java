713d86f68486f3d9fca2f2145e4c722034545615
ItsAyushSomani <ayush.cci@gmail.com>
2021-03-11 16:38:19 +0530

    DS - Array -> ArrayUtil

---
+++ src/Arrays/ArrayUtils.java
@@ -0,0 +1,16 @@
+package Arrays;
+
+/**
+ *
+ */
+
+public class ArrayUtils {
+
+
+    public static void printArray(int arr[]) {
+        for (int i = 0; i < arr.length; i++)
+            System.out.print(arr[i] + " ");
+
+        System.out.println();
+    }
+}
