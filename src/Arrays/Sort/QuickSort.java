8572b3da0b7b40044baf0abcc1b14c90f562f7bc
ItsAyushSomani <ayush.cci@gmail.com>
2021-03-14 19:35:28 +0530

    DS - Array -> Rearrangment

--- src/Arrays/Sort/QuickSort.java
+++ src/Arrays/Sort/QuickSort.java
@@ -1,5 +1,7 @@
 package Arrays.Sort;
 
+import Arrays.ArrayUtils;
+
 /**
  *
  */
@@ -7,9 +9,45 @@ package Arrays.Sort;
 public class QuickSort {
 
     public static void main(String[] args) {
-
+        int arr[] = {10, 7, 8, 9, 1, 5};
+        int n = arr.length;
+        System.out.println("Orignal array");
+        ArrayUtils.printArray(arr);
+        quickSort(arr, 0, n - 1);
+        System.out.println("Sorted array");
+        ArrayUtils.printArray(arr);
     }
 
-    public static void sort() {
+    public static void quickSort(int[] arr, int start, int end) {
+        if (start < end) {
+            /* pi is partitioning index, arr[pi] is
+              now at right place */
+            int p = partition(arr, start, end);
+            // Recursively sort elements before
+            // partition and after partition
+            quickSort(arr, start, p - 1);
+            quickSort(arr, p + 1, end);
+        }
+    }
+
+    public static int partition(int[] arr, int start, int end) {
+        int pivot = arr[start];
+        while (start <= end) {
+            while (arr[start] < pivot) {
+                start++;
+            }
+            while (arr[end] > pivot) {
+                end--;
+            }
+            if (start <= end) {
+                int temp = arr[start];
+                arr[start] = arr[end];
+                arr[end] = temp;
+                start++;
+                end--;
+            }
+
+        }
+        return start;
     }
 }
