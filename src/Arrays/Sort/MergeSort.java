d26f690ae94442a173b275af4be72714190a9da9
ItsAyushSomani <ayush.cci@gmail.com>
2021-03-16 01:11:07 +0530

    DS - Array -> Sorting

---
+++ src/Arrays/Sort/MergeSort.java
@@ -0,0 +1,52 @@
+package Arrays.Sort;
+/**
+ *
+ */
+
+import Arrays.ArrayUtils;
+
+public class MergeSort {
+
+    public static void main(String[] args) {
+        int arr[] = {12, 11, 13, 5, 6, 7};
+        mergeSort(arr, 0, arr.length - 1);
+        ArrayUtils.printArray(arr);
+    }
+
+    public static void mergeSort(int[] arr, int start, int end) {
+        if (start < end) {
+            int mid = (start + end) / 2;
+            mergeSort(arr, start, mid);
+            mergeSort(arr, mid + 1, end);
+            merge(arr, start, mid, end);
+        }
+    }
+
+    private static void merge(int[] arr, int start, int mid, int end) {
+        int temp[] = new int[end - start + 1];
+        int i = start, j = mid + 1, k = 0;
+        while (i <= mid && j <= end) {
+            if (arr[i] <= arr[j]) {
+                temp[k] = arr[i];
+                i++;
+            } else {
+                temp[k] = arr[j];
+                j++;
+            }
+            k++;
+        }
+        while (i <= mid) {
+            temp[k] = arr[i];
+            i++;
+            k++;
+        }
+        while (j <= end) {
+            temp[k] = arr[j];
+            j++;
+            k++;
+        }
+        for (i = start; i <= end; i += 1) {
+            arr[i] = temp[i - start];
+        }
+    }
+}
