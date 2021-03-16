25c596c1b4f7840637c20ef365096a5a4cd3e966
ItsAyushSomani <ayush.cci@gmail.com>
2021-03-16 11:17:10 +0530

    DS - Array -> Searching

---
+++ src/Arrays/search/Q24FixedPointInArrayValueEqualToIndex.java
@@ -0,0 +1,27 @@
+package Arrays.search;
+/**
+ *
+ */
+
+import Arrays.ArrayUtils;
+
+public class Q24FixedPointInArrayValueEqualToIndex {
+
+    public static void main(String[] args) {
+        int arr[] = {-10, -1, 0, 3, 10, 11, 30, 50, 100};
+        System.out.println("Fixed Point is "
+                + Search(arr, 0, arr.length - 1));
+    }
+
+    public static int Search(int[] arr, int start, int end) {
+        if (end >= start) {
+            int mid = start + (end - start) / 2;
+            if (arr[mid] == mid) return mid;
+            if (mid > arr[mid])
+                return Search(arr, (mid + 1), end);
+            else
+                return Search(arr, start, (mid - 1));
+        }
+        return -1;
+    }
+}
