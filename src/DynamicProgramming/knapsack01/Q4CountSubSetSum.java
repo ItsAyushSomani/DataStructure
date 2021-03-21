2255f628739f0d28358b564952b98c93253abe9e
ItsAyushSomani <ayush.cci@gmail.com>
2021-03-21 23:55:34 +0530

    DynamicProgramming

--- src/DynamicProgramming/knapsack01/Q4CountSubSetSum.java
+++ src/DynamicProgramming/knapsack01/Q4CountSubSetSum.java
@@ -1,4 +1,4 @@
-package DynamicProgramming;
+package DynamicProgramming.knapsack01;
 /**
  *
  */
--- src/DynamicProgramming/knapsack01/Q5MinimumSubsetSumDiffrence.java
+++ src/DynamicProgramming/knapsack01/Q5MinimumSubsetSumDiffrence.java
@@ -1,4 +1,4 @@
-package DynamicProgramming;
+package DynamicProgramming.knapsack01;
 /**
  * Given a set of integers, the task is to divide it into two sets S1 and S2 such that the absolute difference between their sums is minimum.
  * If there is a set S with n elements, then if we assume Subset1 has m elements, Subset2 must have n-m elements and the value of abs(sum(Subset1) – sum(Subset2)) should be minimum.
