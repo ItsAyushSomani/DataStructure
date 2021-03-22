288491f23d7c3fb2125d34229cc6b5062c7fa17c
ItsAyushSomani <ayush.cci@gmail.com>
2021-03-23 00:18:13 +0530

    DynamicProgrammin - LCS

--- src/DynamicProgramming/LCS/Q1LCS.java
+++ src/DynamicProgramming/LCS/Q1LCS.java
@@ -1,6 +1,26 @@
 package DynamicProgramming.LCS;
 /**
+ *We have discussed Overlapping Subproblems and Optimal Substructure properties in Set 1 and Set 2 respectively.
+ *  We also discussed one example problem in Set 3. Let us discuss Longest Common Subsequence (LCS) problem as one more example problem that can be solved using Dynamic Programming.
  *
+ * LCS Problem Statement: Given two sequences, find the length of longest subsequence present in both of them.
+ * A subsequence is a sequence that appears in the same relative order, but not necessarily contiguous. For example, “abc”, “abg”, “bdf”, “aeg”, ‘”acefg”, .. etc are subsequences of “abcdefg”.
+ *
+ * In order to find out the complexity of brute force approach, we need to first know the number of possible
+ * different subsequences of a string with length n, i.e., find the number of subsequences with lengths ranging
+ * from 1,2,..n-1. Recall from theory of permutation and combination that number of combinations with 1 element
+ * are nC1. Number of combinations with 2 elements are nC2 and so forth and so on.
+ * We know that nC0 + nC1 + nC2 + … nCn = 2n. So a string of length n has 2n-1 different possible subsequences
+ * since we do not consider the subsequence with length 0. This implies that the time complexity of the brute
+ * force approach will be O(n * 2n). Note that it takes O(n) time to check if a subsequence is common to
+ * both the strings. This time complexity can be improved using dynamic programming.
+ *
+ * It is a classic computer science problem, the basis of diff (a file comparison program that outputs the
+ * differences between two files), and has applications in bioinformatics.
+ *
+ * Examples:
+ * LCS for input Sequences “ABCDGH” and “AEDFHR” is “ADH” of length 3.
+ * LCS for input Sequences “AGGTAB” and “GXTXAYB” is “GTAB” of length 4.
  */
 
 import Arrays.PrintMatrix;
@@ -69,13 +89,17 @@ public class Q1LCS {
                     t1[i][j] = 0;
                 }
                 if (arr[i - 1] == brr[j - 1]) {
-                    t1[i][j] = 1 + t[i - 1][j - 1];
+                    t1[i][j] = 1 + t1[i - 1][j - 1];
                 } else {
                     t1[i][j] = Math.max(t1[i - 1][j], t1[i][j - 1]);
                 }
             }
         }
         PrintMatrix.printMatrix(t1);
-        return t[n][m];
+        return t1[n][m];
     }
 }
+
+
+
+
---
+++ src/DynamicProgramming/LCS/Q2LongestCommonSubString.java
@@ -0,0 +1,44 @@
+package DynamicProgramming.LCS;
+/**
+ *
+ */
+
+import Arrays.ArrayUtils;
+import Arrays.PrintMatrix;
+
+public class Q2LongestCommonSubString {
+
+    public static void main(String[] args) {
+        String X = "OldSite:GeeksforGeeks.org";
+        String Y = "NewSite:GeeksQuiz.com";
+        int m = X.length();
+        int n = Y.length();
+        System.out.println(LCSubStr(X.toCharArray(),
+                Y.toCharArray(), m,
+                n));
+    }
+
+    public static int LCSubStr(char[] X, char[] Y, int n, int m) {
+        int t1[][] = new int[n + 1][m + 1];
+        //initalization can be ommited as by default arr is initialized to 0
+        //    Arrays.fill(t1[0], -1);
+        //    for (int i=0; i< m+1; i++) {
+        //        t[0][i] = 0;
+        //    }
+        int result = 0;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (i == 0 || j == 0) {
+                    t1[i][j] = 0;
+                } else if (Y[j - 1] == X[i - 1]) {
+                    t1[i][j] = 1 + t1[i - 1][j - 1];
+                    result = Math.max(result, t1[i][j]);
+                } else {
+                    t1[i][j] = 0;
+                }
+            }
+        }
+        PrintMatrix.printMatrix(t1);
+        return result;
+    }
+}
