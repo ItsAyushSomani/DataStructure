44af48ce8c1b3fd24e01d5807fab09b95e17060b
ItsAyushSomani <ayush.cci@gmail.com>
2021-06-22 12:14:52 +0530

    Leet Code

--- src/LeetCode/Easy/Q100SameTree.java
+++ src/LeetCode/Easy/Q100SameTree.java
@@ -4,28 +4,26 @@ package LeetCode.Easy;
  *
  * Two binary trees are considered the same if they are structurally identical, and the nodes have the same value.
  */
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
 
-public class Q100SameTree {
-
-    public class TreeNode {
-        int val;
-        TreeNode left;
-        TreeNode right;
-
-        TreeNode() {
-        }
-
-        TreeNode(int val) {
-            this.val = val;
-        }
-
-        TreeNode(int val, TreeNode left, TreeNode right) {
-            this.val = val;
-            this.left = left;
-            this.right = right;
-        }
+    TreeNode() {
     }
 
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class Q100SameTree {
     public boolean isSameTree(TreeNode p, TreeNode q) {
         return isSame(p, q);
     }
