e7a477a32e8903a9645a88b29dc0c386b5f50d82
ItsAyushSomani <ayush.cci@gmail.com>
2021-06-24 09:38:49 +0530

    Leet Code

--- src/LeetCode/Medium/Q29DivideTwoNumbers.java
+++ src/LeetCode/Medium/Q29DivideTwoNumbers.java
@@ -11,6 +11,9 @@ public class Q29DivideTwoNumbers {
     }
 
     public static int divide(int dividend, int divisor) {
+        if (dividend == -2147483648 && divisor == -1) {
+            return 2147483647;
+        }
         int i = 0;
         int abs = 1;
         if (dividend < 0 ^ divisor < 0) {
