16938b7e724bb0f2fda0ec6cc940a541984c1e87
ItsAyushSomani <ayush.cci@gmail.com>
2021-06-27 11:41:09 +0530

    Leet Code

---
+++ src/LeetCode/Easy/Q119PascalTriangle2.java
@@ -0,0 +1,52 @@
+package LeetCode.Easy;
+
+import java.util.ArrayList;
+import java.util.List;
+//Given an integer numRows, return the first numRows of Pascal's triangle.
+//
+// In Pascal's triangle, each number is the sum of the two numbers directly abov
+//e it as shown:
+//
+//
+// Example 1:
+// Input: numRows = 5
+//Output: [[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]]
+// Example 2:
+// Input: numRows = 1
+//Output: [[1]]
+//
+//
+// Constraints:
+//
+//
+// 1 <= numRows <= 30
+//
+// Related Topics Array Dynamic Programming
+// 👍 2859 👎 140
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Q119PascalTriangle2 {
+    public static void main(String[] args) {
+        System.out.println(getRow(5));
+    }
+
+    public static List<Integer> getRow(int numRows) {
+        List<List<Integer>> outerList = new ArrayList<>();
+        outerList.add(new ArrayList<>());
+        outerList.get(0).add(1);
+        for (int i = 1; i <= numRows; i++) {
+            List<Integer> innerList = new ArrayList<>(i);
+            List<Integer> prevRow = outerList.get(i - 1);
+            innerList.add(1);
+            for (int j = 1; j < i; j++) {
+                innerList.add(prevRow.get(j - 1) + prevRow.get(j));
+
+            }
+            innerList.add(1);
+            outerList.add(innerList);
+        }
+        return outerList.get(numRows);
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
