1c1a4eb3a8f230552608b1ac557a2e268a683c99
Ayush Somani <ayush.a.somani@oracle.com>
2024-12-16 23:32:50 +0530

    1448. Count Good Nodes in Binary Tree

---
+++ src/LeetCode/Medium/Challange/Day1/Q1448CountGoodNodesInBinaryTree.java
@@ -0,0 +1,56 @@
+package LeetCode.Medium.Challange.Day1;
+
+
+//Given a binary tree root, a node X in the tree is named good if in the path from root to X there are no nodes with a value greater than X.
+//
+//Return the number of good nodes in the binary tree.
+//
+//
+//
+//Example 1:
+//
+//
+//
+//Input: root = [3,1,4,3,null,1,5]
+//Output: 4
+//Explanation: Nodes in blue are good.
+//        Root Node (3) is always a good node.
+//Node 4 -> (3,4) is the maximum value in the path starting from the root.
+//        Node 5 -> (3,4,5) is the maximum value in the path
+//Node 3 -> (3,1,3) is the maximum value in the path.
+//        Example 2:
+//
+//
+//
+//Input: root = [3,3,null,4,2]
+//Output: 3
+//Explanation: Node 2 -> (3, 3, 2) is not good, because "3" is higher than it.
+//Example 3:
+//
+//Input: root = [1]
+//Output: 1
+//Explanation: Root is considered as good.
+
+import GeeksForGeeks.Trees.BinaryTrees.Introduction.TreeNode;
+
+public class Q1448CountGoodNodesInBinaryTree {
+
+    public int goodNodes(TreeNode root) {
+        return root == null ? 0 : countGoodNodes(root, root.val);
+    }
+
+    public int countGoodNodes(TreeNode root, int rootValue) {
+      
+        if(root == null) {
+            return 0;
+        }
+        var goodNodeCount = 0;
+        if(root.val >= rootValue) {
+            goodNodeCount++;
+            rootValue = root.val;
+        }
+        goodNodeCount += countGoodNodes(root.left, rootValue);
+        goodNodeCount +=  countGoodNodes(root.right, rootValue);
+        return goodNodeCount;
+    }
+}
